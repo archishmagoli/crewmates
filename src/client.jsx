715a0937180d2c5ed67da8636688085224128992
archishmagoli <agoli6@gatech.edu>
2024-04-14 19:40:02 -0400

    update wording?

--- src/client.jsx
+++ src/client.jsx
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-const URL = process.env.PROJECT_URL;
-const API_KEY = process.env.API_KEY;
+const PROJECT_URL = import.meta.env.PROJECT_URL;
+const API_KEY = import.meta.env.API_KEY;
 
-export const supabase = createClient(URL, API_KEY);
+export const supabase = createClient(PROJECT_URL, API_KEY);
