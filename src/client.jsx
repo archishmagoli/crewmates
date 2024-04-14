fd670c391de487cff6776904aba07818bf460b84
archishmagoli <agoli6@gatech.edu>
2024-04-14 19:45:58 -0400

    wording again??

--- src/client.jsx
+++ src/client.jsx
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-const PROJECT_URL = import.meta.env.PROJECT_URL;
-const API_KEY = import.meta.env.API_KEY;
+const VITE_PROJECT_URL = import.meta.env.VITE_PROJECT_URL;
+const VITE_API_KEY = import.meta.env.VITE_API_KEY;
 
-export const supabase = createClient(PROJECT_URL, API_KEY);
+export const supabase = createClient(VITE_PROJECT_URL, VITE_API_KEY);
